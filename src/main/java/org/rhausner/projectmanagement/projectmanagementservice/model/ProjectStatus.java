2b7f7db1cdbe0278cd4976c5987033f63d6b82aa
romanhausner <roman.hausner@gmx.net>
2026-01-11 14:50:51 +0100

    Added json converter for project status.

--- src/main/java/org/rhausner/projectmanagement/projectmanagementservice/model/ProjectStatus.java
+++ src/main/java/org/rhausner/projectmanagement/projectmanagementservice/model/ProjectStatus.java
@@ -1,8 +1,27 @@
 package org.rhausner.projectmanagement.projectmanagementservice.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
 public enum ProjectStatus {
     PLANNED,
     IN_PROGRESS,
     COMPLETED,
-    CANCELLED
+    CANCELLED;
+
+    @JsonCreator
+    public static ProjectStatus fromValue(String value) {
+        return Arrays.stream(values())
+                .filter(v -> v.name().equalsIgnoreCase(value.replace("-", "_")))
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Unknown status: " + value));
+    }
+
+    @JsonValue
+    public String toValue() {
+        return name().toUpperCase();
+    }
 }
